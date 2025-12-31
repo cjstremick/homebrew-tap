53c8f297c5f4d204c9e38416572dc634caec1268
Cj Stremick <cj.stremick@watco.com>
2025-12-30 20:15:34 -0600

    Add VibeShot v1.0.0

---
+++ Casks/vibeshot.rb
@@ -0,0 +1,11 @@
+cask "vibeshot" do
+  version "1.0.0"
+  sha256 "f601c8892a9870f3dc11acd7dfffa4b008fd0cb2ef1aa0c4a221df77059a74a2"
+
+  url "https://github.com/cjstremick/VibeShotMac/releases/download/v1.0.0/VibeShot.zip"
+  name "VibeShot"
+  desc "Screenshot and markup tool"
+  homepage "https://github.com/cjstremick/VibeShotMac"
+
+  app "VibeShot.app"
+end
